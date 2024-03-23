97a71c7e87a4913041c7af4e4ed00b34e142fff7
Đỗ Mai Như Quỳnh <21520429@gm.uit.edu.vn>
2024-03-23 20:05:47 +0700

    Add login screen + assets
    Add new login screen
    Add assets

---
+++ colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+    white: '#ffffff',
+    black: '#000000',
+    blue: '#008dda',
+    lightBlue: '#2cb4ff',
+    red: '#dd1414',
+    gray: '#d9d9d9',
+    darkGray: '#a6a6a6',
+    green: '#46cd01',
+}
