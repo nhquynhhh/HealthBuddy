60d7aaa958bffe569247f314b102668ca132049f
Đỗ Mai Như Quỳnh <21520429@gm.uit.edu.vn>
2024-03-25 12:20:57 +0700

    Add forgot password screen
    Forgot password + OTP password + Reset password + assets

---
+++ screens/reset_password/reset_password.jsx
@@ -0,0 +1,155 @@
+import { View, Text, Image, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, useWindowDimensions, Alert } from 'react-native'
+import React, { Component, useEffect, useState } from 'react'
+import { Icon, Button, Divider } from "react-native-elements";
+import { LinearGradient } from 'expo-linear-gradient';
+import { colors } from '../../colors';
+
+export default function ResetPassword() {
+    const windowHeight = useWindowDimensions().height;
+    const windowWidth = useWindowDimensions().width;
+
+    const [password, setPassword] = React.useState("");
+    const [passwordConfirm, setPasswordConfirm] = React.useState("");
+  
+    const [passwordIsVisible, setPasswordIsVisible] =
+      React.useState(false);
+
+    const [passwordConfirmIsVisible, setPasswordConfirmIsVisible] =
+    React.useState(false);
+
+
+    return (
+        <View style={{alignItems: 'center', paddingTop: 80}}>
+            <Image source={require('../../assets/img_reset_password.png')} 
+                style={{width: 250, height: 250}}>
+            </Image>
+            <Text style={styles.headingText}>Đặt lại mật khẩu</Text>
+            <Text style={[styles.infoText, {textAlign: 'center', lineHeight: 22}]}>Đặt mật khẩu mới cho tài khoản của bạn.</Text>
+
+            <Text style={[styles.describeText, {marginTop: 30}]}>Nhập mật khẩu mới</Text>
+            <SafeAreaView style={styles.inputFieldContainer}>
+                <Icon style={styles.iconBlue} 
+                    name="lock-outline" 
+                    type='material-community' 
+                    color={colors.blue}>
+                </Icon>
+                <TextInput style={styles.inputField} 
+                    placeholder='Nhập mật khẩu' 
+                    maxLength={20}
+                    onChangeText={setPassword}
+                    value={password} 
+                    secureTextEntry={!passwordIsVisible}>
+                </TextInput>
+                <TouchableOpacity
+                    style={styles.passwordVisibleButton}
+                    onPress={() => setPasswordIsVisible(!passwordIsVisible)}
+                >
+                    <Icon
+                        name={passwordIsVisible ? "eye-outline" : "eye-off-outline"}
+                        size={24}
+                        color={colors.blue}
+                        type='ionicon'
+                    />
+                </TouchableOpacity>
+            </SafeAreaView>
+
+            <Text style={styles.describeText}>Xác nhận mật khẩu</Text>
+            <SafeAreaView style={styles.inputFieldContainer}>
+                <Icon style={styles.iconBlue} 
+                    name="lock-outline" 
+                    type='material-community' 
+                    color={colors.blue}>
+                </Icon>
+                <TextInput style={styles.inputField} 
+                    placeholder='Xác nhận mật khẩu' 
+                    onChangeText={setPasswordConfirm} 
+                    value={passwordConfirm} 
+                    secureTextEntry={!passwordConfirmIsVisible}>
+                </TextInput>
+                <TouchableOpacity
+                    style={styles.passwordVisibleButton}
+                    onPress={() => setPasswordConfirmIsVisible(!passwordConfirmIsVisible)}
+                >
+                    <Icon
+                        name={passwordConfirmIsVisible ? "eye-outline" : "eye-off-outline"}
+                        size={24}
+                        color={colors.blue}
+                        type='ionicon'
+                    />
+                </TouchableOpacity>
+            </SafeAreaView>
+
+            <Button title={"XÁC NHẬN"} 
+                style={styles.btnClick} 
+                titleStyle={{fontWeight:'700', fontSize: 20}} 
+                buttonStyle={{minWidth:'95%', height: 42, borderRadius: 10, marginTop: 15}}
+                ViewComponent={LinearGradient} 
+                linearGradientProps={{
+                    colors: [colors.blue, colors.lightBlue],
+                    start: { x: 0, y: 0.5 },
+                    end: { x: 1, y: 0.5 },
+                }}>
+            </Button>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headingText:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 0,
+        color: colors.black
+    },
+    infoText:{
+        color: colors.darkGray, 
+        marginTop: 5,
+    },
+    inputFieldContainer:{
+        backgroundColor: colors.white,
+        maxWidth: '90%',
+        minWidth: '90%',
+        borderColor: colors.blue,
+        borderWidth: 1,
+        borderRadius: 10,
+        height: 42,
+        padding: 5,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    passwordVisibleButton: {
+        position: "absolute",
+        right: 0,
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    inputField:{
+        fontSize: 15,
+    },
+    iconBlue:{
+        color: colors.blue,
+        fontSize: 24,
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    btnClick:{
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 15,
+    },
+    container:{
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        paddingTop: 70
+    }, 
+    describeText:{
+        color: colors.black,
+        fontSize: 17,
+        fontWeight: 'bold',
+        textAlign: 'left',
+        alignSelf: 'flex-start',
+        marginLeft: 25,
+        marginVertical: 14
+    }
+})
