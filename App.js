0a1a6261354b01c915dc25828e30fe2c29b22d39
Đỗ Mai Như Quỳnh <21520429@gm.uit.edu.vn>
2024-03-25 12:21:05 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/login/login';
+import Signup from './screens/signup/signup';
+import Activation from './screens/activation/activation';
+import ForgotPassword from './screens/forgot_password/forgot_password';
+import OtpPassword from './screens/otp_password/otp_password';
+import ResetPassword from './screens/reset_password/reset_password';
 
 export default function App() {
   return (
     <View>
-      <Login></Login>
+      <ResetPassword />
       <StatusBar style="auto" />
     </View>
   );
