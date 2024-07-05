f773c6f986cff4788bd43744d0b492f050559011
Hoàng Mạnh Thắng <21521428@gm.uit.edu.vn>
2024-07-05 12:15:17 +0700

    Update scan image

---
+++ screens/preview/preview.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+import { Button } from 'react-native-elements';
+import * as MediaLibrary from 'expo-media-library';
+import { handleDetect } from '../../services/detect/handle_detect';
+
+export default function PreviewScreen({ route, navigation }) {
+	const { imageUri } = route.params;
+
+	const handleDetectObject = async () => {
+		console.log('Detecting object...');
+		const object = await handleDetect({ uri: imageUri });
+		console.log(object);
+	}
+
+	const retakePicture = () => {
+		navigation.goBack();
+	}
+
+	return (
+		<View style={styles.container}>
+			<Image source={{ uri: imageUri }} style={styles.previewImage} />
+
+			<View style={styles.previewControls}>
+				<Button title="Chọn lại" onPress={retakePicture} />
+				<Button title="Chọn ảnh này" onPress={()=>{handleDetectObject()}} />
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	previewImage: {
+		width: '100%',
+		height: '80%',
+		resizeMode: 'contain',
+	},
+	previewControls: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginBottom: 100,
+		width: '100%',
+	},
+});
---
+++ services/detect/handle_detect.js
@@ -0,0 +1,14 @@
+import { callDetectAPI } from "../api/api_detect";
+import { getAccessToken } from "../../asyncStorage/auth";
+
+export const handleDetect = async (props) => {
+	try {
+		const accessToken = await getAccessToken();
+		const { uri } = props;
+		const response = await callDetectAPI(uri, accessToken);
+		const data = await response.json();
+		return data.objects[0];
+	} catch (error) {
+		console.error('Error detecting object:', error);
+	}	
+}
